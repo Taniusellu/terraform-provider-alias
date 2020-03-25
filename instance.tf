6fa9b68e9eb3becbac73be02e0e91ea4284ce592
Tatiana Moraru <tatianamoraru1998@gmail.com>
2020-03-25 01:59:14 +0000

    created

--- instance.tf
+++ instance.tf
@@ -1,5 +1,36 @@
 
 resource "aws_instance" "web" {
+  provider= "aws.oregon"
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_instance" "web" {
+  provider= "aws.california"
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_instance" "ohio" {
+  provider= "aws.oregon"
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_instance" "web" {
+  provider= "aws.virginia"
   ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
 
